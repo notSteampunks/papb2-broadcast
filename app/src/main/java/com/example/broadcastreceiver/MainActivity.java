84be697717b34f75520536922678aacc68b91e50
notSteampunks <isnanda.a@mail.ugm.ac.id>
2022-02-16 17:22:36 +0700

    first commit

---
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static CustomReceiver mCustomReceiver = new CustomReceiver();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        registerReceiver(mCustomReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(mCustomReceiver);
+    }
+}
